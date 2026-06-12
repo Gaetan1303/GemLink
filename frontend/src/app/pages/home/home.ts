d7eea90b338a0cea0f61e40fe2e9a15e25c3bc66
Gaetan1303 <gaetan.begue13@gmail.com>
2026-06-12 14:02:38 +0200

    creation de la HomePage avec le header, hero-section et footer implémentés

---
+++ frontend/src/app/pages/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../../shared/shared-module';
+
+
+@Component({
+  selector: 'app-home',
+  imports: [SharedModule, CommonModule],
+  templateUrl: './home.html',
+  styleUrls: ['./home.scss'],
+})
+export class Home {}
