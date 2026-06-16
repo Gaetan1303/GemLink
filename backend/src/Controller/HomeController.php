e0efded11a97a642e4f538b312e9e810796e6f38
Gaetan1303 <gaetan.begue13@gmail.com>
2026-06-16 16:20:18 +0200

    création d'une HomePage et d'une health route

---
+++ backend/src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'status' => 'ok',
+            'message' => 'Mimine est derrière vous !',
+            'docs' => '/api/docs'
+        ]);
+    }
+}
