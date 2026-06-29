5e48b71345ae8fad63be33c45406c3003d855017
Gaetan1303 <gaetan.begue13@gmail.com>
2026-06-29 17:15:00 +0200

    Footer avec les pages Mentions, RGPD, contact, Presse, et Mention

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,35 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Profile } from './pages/user/profile/profile';
 
+
 export const routes: Routes = [
   { path: '', component: Home },
+  {
+    path: 'mentions-legales',
+    loadComponent: () => import('./pages/home/mentions-legales/mentions-legales').then(m => m.MentionsLegales)
+  },
+  {
+    path: 'presse',
+    loadComponent: () => import('./pages/home/presse/presse').then(m => m.Presse)
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./pages/home/contact/contact').then(m => m.Contact)
+  },
+  {
+    path: 'politique-de-confidentialite',
+    loadComponent: () => import('./pages/home/politique-de-confidentialite/politique-de-confidentialite').then(m => m.PolitiqueDeConfidentialite)
+  },
+  {
+    path: 'rgpd',
+    loadComponent: () => import('./pages/home/rgpd/rgpd').then(m => m.Rgpd)
+  },
+
   {
     path: 'users/me',
     loadComponent: () => import('./pages/user/profile/profile').then(m => m.Profile)
   },
+
   {
     path: 'auth',
     children: [
--- frontend/src/app/shared/footer/footer.ts
+++ frontend/src/app/shared/footer/footer.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon'; 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterModule} from "@angular/router";
 
 @Component({
   selector: 'app-footer',
-  imports: [MatIconModule, MatToolbarModule, MatButtonModule, ],
+  imports: [MatIconModule, MatToolbarModule, MatButtonModule, RouterModule],
   templateUrl: './footer.html',
   styleUrls: ['./footer.scss'],
 })
